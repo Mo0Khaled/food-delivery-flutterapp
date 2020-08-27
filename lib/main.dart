67bf8e4f4707df362e161d47f07a8b437e58ab98
Mo <mohamedkx77@gmail.com>
2020-08-27 03:49:06 +0200

    Finish product controller , model , apis

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
+import 'package:delivery_food/locator.dart';
+import 'package:delivery_food/screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/product_provider.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  setUpLocator();
   runApp(FoodDelivery());
 }
 
 class FoodDelivery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Food',
-      theme: ThemeData(),
-      home: Scaffold(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => locator<ProductProvider>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Food',
+        theme: ThemeData(),
+        home: HomePage(),
+      ),
     );
   }
 }
-
